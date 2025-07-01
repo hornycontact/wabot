5c8659e9b0585adb6b803e3cb81b25011fc2ba72
hornycontact <git@horny.contact>
2025-07-01 20:44:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'DomBot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "DomBot",
+  botOwner: 'Mommy ADMIN', // Your name
+  ownerNumber: '31653270579', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "A very dominant bot",
   version: "2.0.6",
 };
 
